f94056764387ad9344f8a24b6e417e843fad12ce
luis dourado <lfaddourado@gmail.com>
2023-04-08 16:49:12 -0300

    feat: add clerck authentication and started localization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
 import { type AppType } from "next/app";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ptBR } from "@clerk/localizations";
+
+const localization = {
+  ...ptBR,
+  signIn: {
+    start: {
+      title: "Olá!",
+      subtitle: "Entre com seu melhor SSO para continuar",
+      actionText: "Não tem uma conta?",
+      actionLink: "Crie uma!",
+    },
+  },
+};
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider localization={localization} {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);

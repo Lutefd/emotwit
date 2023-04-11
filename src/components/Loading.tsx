6461b9ee2d9bb8450b4281cd3c2ea4979686476b
luis dourado <lfaddourado@gmail.com>
2023-04-11 07:44:21 -0300

    feat: refactor loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export const LoadingEmoji = () => {
+  return (
+    <div className="loading-animation grid h-screen animate-bounce place-items-center overflow-x-hidden text-[6rem]">
+      🙃
+    </div>
+  );
+};
